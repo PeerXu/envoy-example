46784e6be8de75ae37d667adab1452e181f46982
Peer Xu <pppeerxu@gmail.com>
2018-03-29 11:34:42 +0800

    :sparkles: add tracing with zipkin for calling

--- google-grpc-client/script/greet_start.sh
+++ google-grpc-client/script/greet_start.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
+#! /usr/bin/env sh
 
 greet serve --bind 127.0.0.1:5556 &
-/usr/local/bin/envoy -c /etc/greet-grpc-envoy.yaml
+/usr/local/bin/envoy -c /etc/greet-grpc-envoy.yaml --service-cluster greet
